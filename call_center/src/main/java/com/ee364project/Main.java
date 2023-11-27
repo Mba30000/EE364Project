6c37015ca9312003e69453993fcb444e31f69693
Mahdi <mbathallath@stu.kau.edu.sa>
2023-11-27 20:43:24 +0300

    First Edit

--- call_center/src/main/java/com/ee364project/Main.java
+++ call_center/src/main/java/com/ee364project/Main.java
@@ -4,7 +4,7 @@ import com.ee364project.file_manage.Csv;
 
 public class Main {
     public static void main(String[] args) {
-        HasData[] data = Csv.read("call_center/output/Customer.csv");
+        HasData[] data = Csv.read("C:\\Users\\sbath\\Desktop\\GitHub\\EE364Project\\call_center\\src\\main\\java\\com\\ee364project\\Problem.csv");
         for (HasData datum : data) {
             System.out.println(datum);
         }
